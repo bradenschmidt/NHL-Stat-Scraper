77a42deb0306416588b6f15c914b0205bbae5a8b
Braden Schmidt <bradenschmidt@gmail.com>
2014-12-20 23:02:05 -0600

    Skip mac files. Start to parse the data.

--- nhl-stat-scraper.py
+++ nhl-stat-scraper.py
@@ -13,6 +13,18 @@ stats_players_url = '/ice/playerstats.htm'
 response = requests.get(root_url + stats_players_url)
 
 soup = bs4.BeautifulSoup(response.text)
-stats_players_table = soup.select('div.contentBlock table.data.stats')
+stats_players_table = soup.find('table', {'class': ['data', 'stats']})
+# print(stats_players_table)
 
-print(stats_players_table)
+stats_players_table_body = stats_players_table.find('tbody')
+
+
+stats_players_rows = stats_players_table_body.find_all('tr')
+
+data = []
+for row in stats_players_rows:
+    cols = row.find_all('td')
+    cols = [ele.text.strip() for ele in cols]
+    data.append([ele for ele in cols if ele])  # Get rid of empty values
+
+print(data)
